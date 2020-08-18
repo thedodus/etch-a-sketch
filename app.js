9403a535b47aa623ea2134e1e08c9273e81a7d0b
Bradley Lacke <thedodus@gmail.com>
2020-08-18 14:54:39 -0400

    resize grid working properly

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const container = document.querySelector(".container");
 
+const resize = document.querySelector("#resize");
+
+makeGrid(16, 16);
+draw();
 
 function makeGrid(rows, columns){
+    document.documentElement.style.setProperty("--grid-rows", rows);
+    document.documentElement.style.setProperty("--grid-columns", columns);
     for (let i = 0; i < rows * columns; i++){
         const dot = document.createElement("div");
         container.appendChild(dot);
@@ -9,13 +15,30 @@ function makeGrid(rows, columns){
     }
 }
 
-makeGrid(16, 16);
+function erase(){
+    const dots = document.querySelectorAll(".grid-dot")
+    dots.forEach(dot => dot.classList.remove("draw"));
+}
 
-const dots = document.querySelectorAll(".grid-dot");
+function resizeGrid(){
+    erase();
+    container.innerHTML = "";
+    let newSize = prompt("How many squares would you like on each side of your Etch a Sketch?", "16");
+    makeGrid(newSize, newSize);
+    draw();
+}
 
-dots.forEach(dot => {
-    dot.addEventListener("mouseover", function(){
-        dot.classList.add("draw");
+function draw(){
+    const dots = document.querySelectorAll(".grid-dot");
+
+    dots.forEach(dot => {
+        dot.addEventListener("mouseover", function(){
+            dot.classList.add("draw");
+        })
     })
-})
+}
+
+
+resize.addEventListener("click", resizeGrid);
+
 
